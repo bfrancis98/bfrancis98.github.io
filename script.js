386ee337167cc8ad943a053b1f157a1f7cffcd64
bfrancis98 <21227446+bfrancis98@users.noreply.github.com>
2025-07-26 20:04:40 -0400

    index kinda set up?
    i think i need to change the navs to match index. i also need to change the mains to match the index main.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Toggle mobile menu
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', ()=> {
+    const isMenuOpen = !mobileMenu.classList.contains('translate-x-full');
+
+    if (isMenuOpen) {
+        // Close menu
+        mobileMenu.classList.add('translate-x-full');
+        mobileMenuButton.classList.remove('-translate-x-64');
+    } else {
+        // Open menu
+        mobileMenu.classList.remove('translate-x-full');
+        mobileMenuButton.classList.add('-translate-x-64');
+    }
+});
+
+// Close mobile menu when clicking outside
+document.addEventListener('click', (e) => {
+    if (!mobileMenuButton.contains(e.target) && !mobileMenu.contains(e.target)) {
+        mobileMenu.classList.add('translate-x-full');
+        mobileMenuButton.classList.remove('-translate-x-64');
+    }
+});
+
 const projects = []; // This should be loaded from json or api
 class ProjectFilter {
     constructor() {
